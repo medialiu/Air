a17763c13892dd376721b5a92ae223ce2032a2f8
medialiu <2218103801@qq.com>
2020-06-28 11:17:03 +0800

    双缓冲

--- src/com/media/demo/san/Test.java
+++ src/com/media/demo/san/Test.java
@@ -1,12 +1,24 @@
 package com.media.demo.san;
 
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
 public class Test {
     String name;
     String id;
     int add;
+    //双缓冲技术
+    public void paint(Graphics g){
+        BufferedImage bufferImage=new BufferedImage(300,400,BufferedImage.TYPE_INT_ARGB);
+        Graphics g1=bufferImage.createGraphics();
+        g.drawImage(bufferImage,300,400,null);
+    }
+
+
     //current
 //    System.exit(0);
     public static void main(String[] args) {
+
         System.out.println(45);
     }
 }
